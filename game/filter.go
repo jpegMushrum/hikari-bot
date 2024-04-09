edb2ef7812d203510996f65e3b564374155527c9
bakalover <kostya@shprenger.com>
2024-04-09 18:03:13 +0300

    Filters for Kana

--- game/filter.go
+++ game/filter.go
@@ -14,18 +14,95 @@ const (
 	Noun    = "noun"
 )
 
-// Broken -> Fix
-func GetLastKana(s string) int32 { // -> Check small kana
-	for i := len(s) - 1; i >= 0; i-- {
-		if unicode.In(rune(s[i]), unicode.Hiragana, unicode.Katakana, unicode.Han) {
-			return rune(s[i])
+var SmallKana = []rune{
+	'ォ', 'ぉ', 'ァ', 'ぁ', 'ゥ', 'ぅ', 'ェ',
+	'ぇ', 'ィ', 'ぃ', 'ャ', 'ゃ', 'ョ', 'ょ',
+}
+
+func IsSmall(kana rune) bool {
+	for _, char := range SmallKana {
+		if char == kana {
+			return true
 		}
+	}
+	return false
+}
+
+func MapSmallToBig(kana rune) rune {
+	switch kana {
+	case 'ォ':
+		return 'オ'
+	case 'ぉ':
+		return 'お'
+
+	case 'ァ':
+		return 'ア'
+
+	case 'ぁ':
+		return 'あ'
+
+	case 'ゥ':
+		return 'ウ'
+
+	case 'ぅ':
+		return 'う'
+
+	case 'ェ':
+		return 'エ'
+
+	case 'ぇ':
+		return 'え'
+
+	case 'ィ':
+		return 'イ'
+
+	case 'ぃ':
+		return 'い'
+
+	case 'ャ':
+		return 'ヤ'
+
+	case 'ゃ':
+		return 'や'
+
+	case 'ョ':
+		return 'ヨ'
+
+	case 'ょ':
+		return 'よ'
+
 	}
 	return 0
 }
 
+// Broken -> Fix
+func GetLastKana(s string) int32 {
+	var ans rune = 0
+
+outter_loop:
+	for i := len(s) - 1; i >= 0; i-- {
+		for _, char := range s[i:] { // Oh shit
+			if char == 'ー' { // Doesnt work with Range tables
+				return 'ー'
+			}
+			if unicode.In(char, unicode.Hiragana, unicode.Katakana, unicode.Han) {
+				ans = char
+				break outter_loop
+			}
+		}
+	}
+
+	if IsSmall(ans) {
+		ans = MapSmallToBig(ans)
+	}
+	return ans
+}
+
 func GetFirstKana(s string) int32 {
 	for _, char := range s {
+		if char == 'ー' { // Doesnt work with Range tables
+			return 'ー'
+		}
 		if unicode.In(char, unicode.Hiragana, unicode.Katakana, unicode.Han) {
 			return char
 		}
@@ -42,7 +119,6 @@ func IsEnd(word string) bool {
 
 func IsJapanese(word string) bool {
 	for _, char := range word {
-		// -> fix "ー" sign
 		if !unicode.In(char, unicode.Hiragana, unicode.Katakana, unicode.Han) {
 			return false
 		}
--- game/filter_test.go
+++ game/filter_test.go
@@ -7,6 +7,21 @@ import (
 )
 
 func TestKana(t *testing.T) {
+	assert.True(t, IsSmall('ォ'))
+	assert.True(t, IsSmall('ぁ'))
+	assert.False(t, IsSmall('ア'))
+	assert.False(t, IsSmall('え'))
+	assert.Equal(t, MapSmallToBig('ォ'), 'オ')
+	assert.Equal(t, MapSmallToBig('ゃ'), 'や')
+	assert.Equal(t, MapSmallToBig('ぃ'), 'い')
 	assert.Equal(t, GetFirstKana("へんたい"), 'へ')
 	assert.Equal(t, GetFirstKana("キス"), 'キ')
+	assert.Equal(t, GetFirstKana("ラ"), 'ラ')
+	assert.Equal(t, GetLastKana("へんたい"), 'い')
+	assert.Equal(t, GetLastKana("キス"), 'ス')
+	assert.Equal(t, GetLastKana("ラ"), 'ラ')
+	assert.Equal(t, GetLastKana("スキー"), 'ー')
+	assert.NotEqual(t, GetLastKana("しゅしょ"), 'ょ')
+	assert.Equal(t, GetLastKana("しゅしょ"), 'よ')
+	assert.Equal(t, GetFirstKana("ラ"), GetLastKana("ラ"))
 }
